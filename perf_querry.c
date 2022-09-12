bdbbe194128e4560dfdc97cab766df15fb874f18
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-09-12 15:11:56 +0200

    Updated

--- perf_querry.c
+++ perf_querry.c
@@ -14,38 +14,38 @@ struct info_s info;
 // AGGREGATE TOOLS
 //______________________________________________________________________________
 
-static void aggregate_4bit(uint32_t * dest, uint32_t val)
-{
-    if ((((*dest) + val) < (*dest)) || ((*dest) + val) > 0xf)
-        (*dest) = 0xf;
-    else
-        (*dest) = (*dest) + val;
-}
+// static void aggregate_4bit(uint32_t * dest, uint32_t val)
+// {
+//     if ((((*dest) + val) < (*dest)) || ((*dest) + val) > 0xf)
+//         (*dest) = 0xf;
+//     else
+//         (*dest) = (*dest) + val;
+// }
 
-static void aggregate_8bit(uint32_t * dest, uint32_t val)
-{
-    if ((((*dest) + val) < (*dest))
-        || ((*dest) + val) > 0xff)
-        (*dest) = 0xff;
-    else
-        (*dest) = (*dest) + val;
-}
+// static void aggregate_8bit(uint32_t * dest, uint32_t val)
+// {
+//     if ((((*dest) + val) < (*dest))
+//         || ((*dest) + val) > 0xff)
+//         (*dest) = 0xff;
+//     else
+//         (*dest) = (*dest) + val;
+// }
 
-static void aggregate_16bit(uint32_t * dest, uint32_t val)
-{
-    if ((((*dest) + val) < (*dest))
-        || ((*dest) + val) > 0xffff)
-        (*dest) = 0xffff;
-    else
-        (*dest) = (*dest) + val;
-}
+// static void aggregate_16bit(uint32_t * dest, uint32_t val)
+// {
+//     if ((((*dest) + val) < (*dest))
+//         || ((*dest) + val) > 0xffff)
+//         (*dest) = 0xffff;
+//     else
+//         (*dest) = (*dest) + val;
+// }
 
-static void aggregate_32bit(uint32_t * dest, uint32_t val)
-{
-    if (((*dest) + val) < (*dest))
-        (*dest) = 0xffffffff;
-    else
-        (*dest) = (*dest) + val;
+// static void aggregate_32bit(uint32_t * dest, uint32_t val)
+// {
+//     if (((*dest) + val) < (*dest))
+//         (*dest) = 0xffffffff;
+//     else
+//         (*dest) = (*dest) + val;
 }
 
 // ERR_QUERRY
@@ -150,7 +150,7 @@ static void aggregate_perfcounters(perf_data_t *perf_count)
     mad_decode_field(pc, IB_PC_COUNTER_SELECT_F, &val);
     perf_count->counterselect = val;
     mad_decode_field(pc, IB_PC_ERR_SYM_F, &val);
-    perf_count->symbolerrors = val
+    perf_count->symbolerrors = val;
     mad_decode_field(pc, IB_PC_LINK_RECOVERS_F, &val);
     perf_count->linkrecovers = val;
     mad_decode_field(pc, IB_PC_LINK_DOWNED_F, &val);
